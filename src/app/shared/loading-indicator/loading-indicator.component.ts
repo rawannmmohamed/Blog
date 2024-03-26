6c410db62a5c14edff8f857349af1ddb43384236
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-03-26 21:31:34 +0200

    init structure

---
+++ src/app/shared/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-indicator.component.html',
+  styleUrl: './loading-indicator.component.css'
+})
+export class LoadingIndicatorComponent {
+
+}
