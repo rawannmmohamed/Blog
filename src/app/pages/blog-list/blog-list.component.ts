79f630d86b8d4d1e6d41124873a3bd6b0a297765
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-03-28 00:08:27 +0200

    error & loading handeling

--- src/app/pages/blog-list/blog-list.component.ts
+++ src/app/pages/blog-list/blog-list.component.ts
@@ -1,23 +1,52 @@
-import { Component, EventEmitter, OnInit, output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from '../../core/models/post';
 import { PostService } from '../../core/services/post.service';
 import { PostItemComponent } from '../../components/post-item/post-item.component';
+import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { LoadingIndicatorComponent } from '../../components/loading-indicator/loading-indicator.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [PostItemComponent],
+  imports: [
+    PostItemComponent,
+    ErrorMessageComponent,
+    LoadingIndicatorComponent,
+    FormsModule,
+  ],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.css',
 })
 export class BlogListComponent implements OnInit {
   posts: Post[] = [];
+  errorMessage: boolean = false;
+  searchText: string = '';
+  displayedPosts: Post[] = [];
+  initialDisplayCount: number = 3;
+  loadMoreCount: number = 3;
 
   constructor(private postService: PostService) {}
 
   ngOnInit() {
-    this.postService.getPosts().subscribe((response) => {
-      this.posts = response;
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    this.postService.getPosts().subscribe({
+      next: (response) => {
+        this.posts = response;
+        this.displayedPosts = this.posts.slice(0, this.initialDisplayCount);
+      },
+      error: () => {
+        this.errorMessage = true;
+      },
     });
+  }
+
+  loadMorePosts() {
+    const currentDisplayCount = this.displayedPosts.length;
+    const newDisplayCount = currentDisplayCount + this.loadMoreCount;
+    this.displayedPosts = this.posts.slice(0, newDisplayCount);
   }
 }
